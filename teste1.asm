4d58b0d8e4160422db237f0e977d2dc7a6c3d7ec
icaro <164355795+icarolindogostoso@users.noreply.github.com>
2024-12-22 08:28:07 -0300

    Update teste1.asm

--- teste1.asm
+++ teste1.asm
@@ -31,6 +31,8 @@ desenhar_map_2:
 fim_map_2:
       	lui $8, 0x1001
       	ori $20, 0xffffff
+      	
+      	sw $20, 0($8) 
 
       	lui $21, 0xffff
       	addi $25, $0, 32
@@ -38,7 +40,7 @@ fim_map_2:
       	addi $12, $0, 'd'
 	addi $13, $0, 's'
       	addi $14, $0, 'w'
-principal:      
+principal:
       	lw $22, 0($21)
       	beq $22, $0, cont
       	lw $23, 4($21)
@@ -52,47 +54,48 @@ principal:
       	j cont
       	
 esq:  
-	addi $10, $0, -4
 	sw $20, 0($8)
+	sw $20, -4($8)
+      	lw $9, 4096($8)
+      	sw $9, 0($8)
+      	addi $8, $8, -4
       	jal timer      
-      	lw $9, 2048($8)
-	sw $9, 0($8)
-      	add $8, $8, $10      
       	j cont
      
 dir:  	
 	
-	addi $10, $0, 4
-	add $8, $8, $10
-	add $4 $8 $0
-	jal verificacao
-	bne $2 $0 arroche
 	sw $20, 0($8)
-      	jal timer      
-      	lw $9, 2048($8)
+	sw $20, 4($8)
+      	lw $9, 4096($8)
       	sw $9, 0($8)
+      	add $4 $0 $8
+      	jal verificacao
+      	bne $2 $0 arroche
+      	addi $8, $8, 4
+      	jal timer
       	j cont
       	
 arroche:
-	nop
+	addi $2 $0 10
+	syscall
 	j cont
      
 baixo:  
-	addi $10, $0, +128
 	sw $20, 0($8)
-      	jal timer      
-      	lw $9, 2048($8)
+      	sw $20, 128($8)
+      	lw $9, 4096($8)
       	sw $9, 0($8)
-      	add $8, $8, $10
+      	addi $8, $8, 128
+      	jal timer
       	j cont
      
 cima:  	
-	addi $10, $0, -128
 	sw $20, 0($8)
-      	jal timer      
-      	lw $9, 2048($8)
+      	sw $20, -128($8)
+      	lw $9, 4096($8)
       	sw $9, 0($8)
-      	add $8, $8, $10
+      	addi $8, $8, -128
+      	jal timer
       	j cont         
      
 cont: 
@@ -101,11 +104,11 @@ fim:
 	addi $2, $0, 10
       	syscall    
 #====================================================================
-# função Timer
+# funÃ§Ã£o Timer
 
 timer: sw $16, 0($29)
        addi $29, $29, -4
-       addi $16, $0, 100000
+       addi $16, $0, 10000
 forT:  beq $16, $0, fimT
        nop
        nop
@@ -132,24 +135,28 @@ verificacao:
        	addi $29, $29, -4
        	sw $21, 0($29)
        	addi $29, $29, -4
+       	sw $22, 0($29)
+       	addi $29, $29, -4
        	
 	lui $20 0x1001
 	addi $8 $0 16
 	add $9 $0 $0
-	addi $10 $0 256
-	addi $11 $0 512
+	addi $10 $0 64
+	addi $11 $0 128
 laco_verificacao:
 	beq $9 $8 fim_laco_verificacao
-	mul $12 $11 $9
+	mul $12 $9 $11
 	add $12 $12 $10
 	add $20 $20 $12
-	and $21 $20 $4
-	beq $21 $20 colidiu
+	addi $12 $4 4
+	beq $21 $22 colidiu
 	addi $9 $9 1
 	j laco_verificacao
 fim_laco_verificacao:
 	add $2 $0 $0
 	
+	addi $29, $29, 4                                                    
+       	lw $22, 0($29)
 	addi $29, $29, 4                                                    
        	lw $21, 0($29)
        	addi $29, $29, 4                                                    
@@ -171,6 +178,8 @@ fim_laco_verificacao:
 colidiu:
 	addi $2 $0 1
 	
+	addi $29, $29, 4                                                    
+       	lw $22, 0($29)
 	addi $29, $29, 4                                                    
        	lw $21, 0($29)
        	addi $29, $29, 4                                                    
