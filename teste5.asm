feba729a20170f751fb48387f3640c5964e825b0
icaro <164355795+icarolindogostoso@users.noreply.github.com>
2025-01-03 09:47:01 -0300

    Update teste5.asm

--- teste5.asm
+++ teste5.asm
@@ -554,7 +554,7 @@ detalhe_tijoloH:
 	
 prox_t:
 	beq $11, $0, caixa_init
-	addi $8, $8, 48
+	addi $8, $8, 60
 	li $20, 0xed6408
 	li $9, 11
 	li $10, 11
@@ -565,7 +565,7 @@ caixa_init:
 	lui $8, 0x1001
 	addi $8, $8, 10476
 	li $20, 0xfbbe2e
-	li $9, 11
+	li $9, 14
 	li $10, 11
 
 caixa:
@@ -579,31 +579,32 @@ caixa:
 plc:
 	beq $10, $0, lci
 	addi $10, $10, -1
-	addi $8, $8, 468
-	li $9, 11
+	addi $8, $8, 456
+	li $9, 14
 	j caixa
-	
 lci:
-	li $20, 0xff00ff
-	li $9, 5
+	li $20, 0xfbbe2d
+	li $9, 6
 	addi $8, $8, -4
 lc:
 	beq $9, $0, interro_init
 	sw $20, 0($8)
 	sw $20 65536($8)
-	sw $20 -24($8)
-	sw $20 65512($8)
+	sw $20 -32($8)
+	sw $20 65504($8)
 	
 	addi $8, $8, -4
 	addi $9, $9, -1
 	j lc
+ 
+	
 	
 interro_init:
 	addi $8, $8, 44
 	lui $8, 0x1001
-	addi $8, $8, 11000
+	addi $8, $8, 11004
 	li $20, 0xffffff
-	addi $9 $0 2
+	li $9, 2
 
 interro:
 	beq $9, $0 fim
@@ -4504,7 +4505,11 @@ nascer_cogumelo:
        	sw $13 0($29)
        	addi $29 $29 -4
        	
-	add $8 $4 $0
+	lui $8 0x1001
+	addi $8 $8 16632
+	addi $9 $0 16
+	mul $9 $9 $6
+	sub $8 $8 $9
 	addi $8 $8 -4096
 	addi $8 $8 4
 	
