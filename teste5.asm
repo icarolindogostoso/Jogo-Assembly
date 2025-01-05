08ba07de52cb4cb0b2f4aedb80f551cb59267c0b
icaro <164355795+icarolindogostoso@users.noreply.github.com>
2025-01-05 19:36:53 -0300

    Update teste5.asm

--- teste5.asm
+++ teste5.asm
@@ -30,7 +30,7 @@ menu_level_1:
 	lui $8 0x1001
 	addi $8 $8 22528
 	add $4 $8 $0
-	jal desenhar_personagem_direita
+	jal desenhar_personagem_morrendo
 	
 	lui $9 0xffff
 	addi $10 $0 'a'
@@ -3062,6 +3062,138 @@ fim_pulando_esquerda:
 	jr $31
 	
 #===================================================
+# - funcao para desenhar o personagem morrendo
+
+desenhar_personagem_morrendo:
+	sw $31 0($29)
+       	addi $29 $29 -4
+       	sw $5 0($29)
+       	addi $29 $29 -4
+       	sw $8 0($29)
+       	addi $29 $29 -4
+       	sw $9 0($29)
+       	addi $29 $29 -4
+       	sw $10 0($29)
+       	addi $29 $29 -4
+       	sw $11 0($29)
+       	addi $29 $29 -4
+       	sw $20 0($29)
+       	addi $29 $29 -4
+	
+	add $8 $4 $0
+	addi $9 $0 0x0ec7db
+	
+	addi $10 $0 8
+laco_personagem_morrendo_1:
+	beq $10 $0 fim_laco_personagem_morrendo_1
+	addi $11 $0 8
+laco_personagem_morrendo_2:
+	beq $11 $0 fim_laco_personagem_morrendo_2
+	
+	sw $9 0($8)
+	
+	addi $8 $8 4
+	addi $11 $11 -1
+	j laco_personagem_morrendo_2
+fim_laco_personagem_morrendo_2:
+	addi $8 $8 -32
+	addi $8 $8 512
+	addi $10 $10 -1
+	j laco_personagem_morrendo_1
+fim_laco_personagem_morrendo_1:
+	
+	add $8 $4 $0
+	addi $8 $8 2052
+	li $20, 0xffd7a4
+	li $9, 6
+	li $10, 2
+cabeça_kill:
+	beqz $9 plck
+	
+	sw $20, -512($8)
+	
+	addi $8, $8, 4
+	addi $9, $9, -1
+	j cabeça_kill
+	
+plck:
+	beqz $10 linhas_kill_init
+	subi $10, $10 1
+	addi $8, $8, 488
+	li $9, 6
+	j cabeça_kill
+	
+linhas_kill_init:
+	subi $8, $8, 2580
+	li $9, 2
+	li $10, 0x000001
+	li $11, 0x9a3894 
+	li $12, 0xfdff0e
+	li $21, 0xaa7722
+	li $20, 0xf474a6
+linhas_kill:
+	beqz $9 detalhes_kill
+	
+	sw $12 0($8)
+	sw $12 8($8)
+	sw $12 508($8)
+	sw $12 516($8)
+	sw $12 524($8)
+	
+	sw $21 1532($8)
+	sw $20 1540($8)
+	sw $21 1548($8)
+	
+	sw $12 2572($8)
+	sw $12 2556($8)
+	sw $11 2564($8)
+	sw $11 3076($8)
+	sw $11 3068($8)
+	sw $11 3084($8)
+
+	addi $8, $8 4
+	addi $9, $9 -1
+	j linhas_kill
+detalhes_kill:
+	subi $8, $8, 4
+	li $9, 0x2b7741 
+	li $10, 0x000001
+	li $11, 0xffffff
+	
+	sw $10, 1020($8)
+	sw $10, 1032($8)
+	
+	sw $11 0($8)
+	sw $11 4($8)
+	sw $11 2036($8)
+	sw $11 2064($8)
+	sw $11 2556($8)
+	sw $11 2568($8)
+	
+	sw $9 3088($8)
+	sw $9 3060($8)
+fim_personagem_morrendo:
+	addi $5 $0 8
+	jal apagar_fundo
+
+	addi $29 $29 4                                                    
+       	lw $20 0($29)
+	addi $29 $29 4                                                    
+       	lw $11 0($29)
+	addi $29 $29 4                                                    
+       	lw $10 0($29)
+	addi $29 $29 4                                                    
+       	lw $9 0($29)
+       	addi $29 $29 4                                                    
+       	lw $8 0($29)
+       	addi $29 $29 4                                                    
+       	lw $5 0($29)
+	addi $29 $29 4                                                    
+       	lw $31 0($29)
+       	
+	jr $31
+	
+#===================================================
 # - funcao para desenhar o personagem grande para a direita
 
 desenhar_personagem_grande_direita:
@@ -5713,7 +5845,7 @@ laco_personagem_morrendo_subindo_1:
 	addi $8 $8 -2048
 	addi $10 $8 4096
 	add $4 $8 $0
-	jal desenhar_personagem_direita
+	jal desenhar_personagem_morrendo
 	
 	addi $11 $0 4
 laco_personagem_morrendo_subindo_2:
@@ -5746,7 +5878,7 @@ laco_personagem_morrendo_descendo_1:
 	addi $8 $8 2048
 	addi $10 $8 -2048
 	add $4 $8 $0
-	jal desenhar_personagem_direita
+	jal desenhar_personagem_morrendo
 	
 	addi $11 $0 4
 laco_personagem_morrendo_descendo_2:
