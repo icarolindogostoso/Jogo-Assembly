75ac50f9836f6c4f8686149663d2d4a3ec5bdc8f
icaro <164355795+icarolindogostoso@users.noreply.github.com>
2025-02-14 17:33:52 -0300

    Update jogo_com_nivel_3.asm

--- jogo_com_nivel_3.asm
+++ jogo_com_nivel_3.asm
@@ -413,6 +413,11 @@ iniciar_nivel_3:
 	add $4 $8 $0
 	jal desenhar_personagem_direita
 	
+	lui $13 0x1001
+	addi $13 $13 11712
+	add $4 $13 $0
+	jal desenhar_bullet_bill
+	
 	lui $9 0xffff
 	addi $10 $0 'a'
 	addi $11 $0 'd'
@@ -482,8 +487,12 @@ laco_andar_cima_nivel_3:
 continuacao_cima_nivel_3: 
 	add $4 $20 $0
 	add $5 $8 $0
-	jal mover_boss_personagem_pulando
+	jal mover_boss
 	add $20 $2 $0
+	
+	add $4 $13 $0
+	jal andar_bullet_bill_esquerda
+	add $13 $2 $0
 	
 	add $4 $8 $0
 	jal andar_para_cima_nivel_3
@@ -514,6 +523,10 @@ continuacao_nivel_3:
 	add $5 $8 $0
 	jal mover_boss
 	add $20 $2 $0
+	
+	add $4 $13 $0
+	jal andar_bullet_bill_esquerda
+	add $13 $2 $0
 	
 	j loop_principal_nivel_3
 	
@@ -32316,6 +32329,371 @@ fim_bandeira_desenhar:
        	lw $31 0($29)
        	
 	jr $31
+	
+#===================================================
+# - funcao para desenhar o bullet bill
+
+desenhar_bullet_bill:
+	sw $31 0($29)
+       	addi $29 $29 -4
+       	sw $8 0($29)
+       	addi $29 $29 -4
+       	sw $9 0($29)
+       	addi $29 $29 -4
+       	sw $10 0($29)
+       	addi $29 $29 -4
+       	sw $11 0($29)
+       	addi $29 $29 -4
+       	sw $12 0($29)
+       	addi $29 $29 -4
+       	sw $20 0($29)
+       	addi $29 $29 -4
+       	sw $22 0($29)
+       	addi $29 $29 -4
+       	
+	add $8 $4 $0
+	addi $22 $8 65536
+	addi $9 $0 0x0ec7db
+	
+	addi $10 $0 16
+laco_bullet_bill_1:
+	beq $10 $0 fim_laco_bullet_bill_1
+	addi $11 $0 16
+laco_bullet_bill_2:
+	beq $11 $0 fim_laco_bullet_bill_2
+	
+	sw $9 0($8)
+	sw $9 0($22)
+	
+	addi $8 $8 4
+	addi $22 $22 4
+	addi $11 $11 -1
+	j laco_bullet_bill_2
+fim_laco_bullet_bill_2:
+	addi $8 $8 -64
+	addi $22 $22 -64
+	addi $8 $8 512
+	addi $22 $22 512
+	addi $10 $10 -1
+	j laco_bullet_bill_1
+fim_laco_bullet_bill_1:
+	
+	add $8 $4 $0
+	addi $8 $8 528
+	addi $22 $8 65536
+	
+	addi $9 $0 0x000002
+	
+	addi $10 $0 12
+laco_corpo_bullet_bill_1:
+	beq $10 $0 fim_laco_corpo_bullet_bill_1
+	addi $11 $0 12
+laco_corpo_bullet_bill_2:
+	beq $11 $0 fim_laco_corpo_bullet_bill_2
+	
+	sw $9 0($8)
+	sw $9 0($22)
+	
+	addi $8 $8 4
+	addi $22 $22 4
+	addi $11 $11 -1
+	j laco_corpo_bullet_bill_2
+fim_laco_corpo_bullet_bill_2:
+	addi $8 $8 -48
+	addi $22 $22 -48
+	addi $8 $8 512
+	addi $22 $22 512
+	addi $10 $10 -1
+	j laco_corpo_bullet_bill_1
+fim_laco_corpo_bullet_bill_1:
+	
+	add $8 $4 $0
+	addi $8 $8 1032
+	addi $22 $8 65536
+	
+	addi $10 $0 10
+laco_corpo_2_bullet_bill_1:
+	beq $10 $0 fim_laco_corpo_2_bullet_bill_1
+	addi $11 $0 2
+laco_corpo_2_bullet_bill_2:
+	beq $11 $0 fim_laco_corpo_2_bullet_bill_2
+	
+	sw $9 0($8)
+	sw $9 0($22)
+	
+	addi $8 $8 4
+	addi $22 $22 4
+	addi $11 $11 -1
+	j laco_corpo_2_bullet_bill_2
+fim_laco_corpo_2_bullet_bill_2:
+	addi $8 $8 -8
+	addi $22 $22 -8
+	addi $8 $8 512
+	addi $22 $22 512
+	addi $10 $10 -1
+	j laco_corpo_2_bullet_bill_1
+fim_laco_corpo_2_bullet_bill_1:
+
+	add $8 $4 $0
+	addi $8 $8 1540
+	addi $22 $8 65536
+	
+	addi $10 $0 8
+laco_corpo_3_bullet_bill_1:
+	beq $10 $0 fim_laco_corpo_3_bullet_bill_1
+	
+	sw $9 0($8)
+	sw $9 0($22)
+	
+	addi $8 $8 512
+	addi $22 $22 512
+	addi $10 $10 -1
+	j laco_corpo_3_bullet_bill_1
+fim_laco_corpo_3_bullet_bill_1:
+
+	add $8 $4 $0
+	addi $8 $8 2560
+	addi $22 $8 65536
+	
+	addi $10 $0 4
+laco_corpo_4_bullet_bill_1:
+	beq $10 $0 fim_laco_corpo_4_bullet_bill_1
+	
+	sw $9 0($8)
+	sw $9 0($22)
+	
+	addi $8 $8 512
+	addi $22 $22 512
+	addi $10 $10 -1
+	j laco_corpo_4_bullet_bill_1
+fim_laco_corpo_4_bullet_bill_1:
+	
+	add $8 $4 $0
+	addi $8 $8 24
+	addi $22 $8 65536
+	
+	addi $10 $0 6
+laco_corpo_5_bullet_bill_1:
+	beq $10 $0 fim_laco_corpo_5_bullet_bill_1
+	
+	sw $9 0($8)
+	sw $9 0($22)
+	
+	addi $8 $8 4
+	addi $22 $22 4
+	addi $10 $10 -1
+	j laco_corpo_5_bullet_bill_1
+fim_laco_corpo_5_bullet_bill_1:
+	
+	add $8 $4 $0
+	addi $8 $8 56
+	addi $22 $8 65536
+	
+	addi $10 $0 2
+laco_corpo_6_bullet_bill_1:
+	beq $10 $0 fim_laco_corpo_6_bullet_bill_1
+	
+	sw $9 0($8)
+	sw $9 0($22)
+	
+	addi $8 $8 4
+	addi $22 $22 4
+	addi $10 $10 -1
+	j laco_corpo_6_bullet_bill_1
+fim_laco_corpo_6_bullet_bill_1:
+
+	add $8 $4 $0
+	addi $8 $8 6680
+	addi $22 $8 65536
+	
+	addi $10 $0 5
+laco_corpo_7_bullet_bill_1:
+	beq $10 $0 fim_laco_corpo_7_bullet_bill_1
+	
+	sw $9 0($8)
+	sw $9 0($22)
+	
+	addi $8 $8 4
+	addi $22 $22 4
+	addi $10 $10 -1
+	j laco_corpo_7_bullet_bill_1
+fim_laco_corpo_7_bullet_bill_1:
+
+	add $8 $4 $0
+	addi $8 $8 6708
+	addi $22 $8 65536
+	
+	addi $10 $0 3
+laco_corpo_8_bullet_bill_1:
+	beq $10 $0 fim_laco_corpo_8_bullet_bill_1
+	
+	sw $9 0($8)
+	sw $9 0($22)
+	
+	addi $8 $8 4
+	addi $22 $22 4
+	addi $10 $10 -1
+	j laco_corpo_8_bullet_bill_1
+fim_laco_corpo_8_bullet_bill_1:
+	add $8 $4 $0
+	addi $22 $8 65536
+	li $9 0xfffffe
+	
+	sw $9 540($8)
+	sw $9 544($8)
+	sw $9 548($8)
+	sw $9 552($8)
+	sw $9 556($8)
+	sw $9 1048($8)
+	sw $9 1052($8)
+	sw $9 1056($8)
+	sw $9 1060($8)
+	sw $9 1064($8)
+	sw $9 1068($8)
+	sw $9 1560($8)
+	
+	sw $9 540($22)
+	sw $9 544($22)
+	sw $9 548($22)
+	sw $9 552($22)
+	sw $9 556($22)
+	sw $9 1048($22)
+	sw $9 1052($22)
+	sw $9 1056($22)
+	sw $9 1060($22)
+	sw $9 1064($22)
+	sw $9 1068($22)
+	sw $9 1560($22)
+	
+	sw $9 568($8)
+	sw $9 1080($8)
+	sw $9 1076($8)
+	sw $9 1588($8)
+	
+	sw $9 568($22)
+	sw $9 1080($22)
+	sw $9 1076($22)
+	sw $9 1588($22)
+	
+	sw $9 1552($8)
+	sw $9 2064($8)
+	sw $9 2060($8)
+	sw $9 2576($8)
+	sw $9 2572($8)
+	sw $9 2564($8)
+	sw $9 3088($8)
+	sw $9 3084($8)
+	sw $9 3080($8)
+	sw $9 3076($8)
+	sw $9 3596($8)
+	sw $9 3592($8)
+	
+	sw $9 1552($22)
+	sw $9 2064($22)
+	sw $9 2060($22)
+	sw $9 2576($22)
+	sw $9 2572($22)
+	sw $9 2564($22)
+	sw $9 3088($22)
+	sw $9 3084($22)
+	sw $9 3080($22)
+	sw $9 3076($22)
+	sw $9 3596($22)
+	sw $9 3592($22)
+	
+	add $8 $4 $0
+	addi $8 $8 4120
+	addi $22 $8 65536
+	
+	addi $9 $0 0xffffa0
+	
+	addi $10 $0 3
+laco_braco_bullet_bill_1:
+	beq $10 $0 fim_laco_braco_bullet_bill_1
+	addi $11 $0 6
+laco_braco_bullet_bill_2:
+	beq $11 $0 fim_laco_braco_bullet_bill_2
+	
+	sw $9 0($8)
+	sw $9 0($22)
+	
+	addi $8 $8 4
+	addi $22 $22 4
+	addi $11 $11 -1
+	j laco_braco_bullet_bill_2
+fim_laco_braco_bullet_bill_2:
+	addi $8 $8 -24
+	addi $22 $22 -24
+	addi $8 $8 512
+	addi $22 $22 512
+	addi $10 $10 -1
+	j laco_braco_bullet_bill_1
+fim_laco_braco_bullet_bill_1:
+
+	sw $9 0($8)
+	sw $9 4($8)
+	sw $9 8($8)
+	sw $9 12($8)
+	sw $9 16($8)
+	sw $9 0($22)
+	sw $9 4($22)
+	sw $9 8($22)
+	sw $9 12($22)
+	sw $9 16($22)
+	
+	addi $8 $8 -1024
+	addi $22 $22 -1024
+	
+	sw $9 -4($8)
+	sw $9 508($8)
+	sw $9 -4($22)
+	sw $9 508($22)
+	
+	addi $8 $8 -488
+	addi $22 $22 -488
+	
+	sw $9 0($8)
+	sw $9 512($8)
+	sw $9 0($22)
+	sw $9 512($22)
+	
+	addi $8 $8 -520
+	addi $22 $22 -520
+	
+	sw $9 0($8)
+	sw $9 4($8)
+	sw $9 8($8)
+	sw $9 -512($8)
+	sw $9 -508($8)
+	sw $9 0($22)
+	sw $9 4($22)
+	sw $9 8($22)
+	sw $9 -512($22)
+	sw $9 -508($22)
+	
+fim_desenhar_bullet_bill:
+	addi $5 $0 16
+	jal apagar_fundo
+	
+	addi $29 $29 4  
+	lw $22 0($29)
+	addi $29 $29 4  
+	lw $20 0($29)
+	addi $29 $29 4  
+	lw $12 0($29)
+	addi $29 $29 4  
+	lw $11 0($29)
+	addi $29 $29 4                                                    
+       	lw $10 0($29)
+	addi $29 $29 4                                                    
+       	lw $9 0($29)
+       	addi $29 $29 4                                                    
+       	lw $8 0($29)
+	addi $29 $29 4                                                    
+       	lw $31 0($29)
+       	
+	jr $31
+	
 #===================================================
 # - funcao para andar para a esquerda
 # - registradores de entrada: $4
@@ -33955,13 +34333,6 @@ fim_laco_2_andar_baixo_nivel_3:
 	addi $10 $10 -1
 	j laco_1_andar_baixo_nivel_3
 fim_laco_1_andar_baixo_nivel_3:
-	
-	jal timer
-	jal timer
-	jal timer
-	jal timer
-	jal timer
-	
 	add $3 $0 $0
 	add $18 $0 $0
 	
@@ -34313,11 +34684,6 @@ fim_laco_2_andar_cima_nivel_3:
 	addi $10 $10 -1
 	j laco_1_andar_cima_nivel_3
 fim_laco_1_andar_cima_nivel_3:
-	jal timer
-	jal timer
-	jal timer
-	jal timer
-	jal timer
 	
 	add $18 $0 $0
 	
@@ -36355,11 +36721,11 @@ mover_boss:
        	sw $9 0($29)
        	addi $29 $29 -4
 
-	addi $8 $0 262144
+	addi $8 $0 18
 	div $4 $8
 	mfhi $9
 	beq $9 $0 mover_boss_1
-	addi $8 $0 131072
+	addi $8 $0 9
 	beq $9 $8 mover_boss_2
 	addi $2 $4 1
 	
@@ -36418,33 +36784,6 @@ continuacao_para_laco_cima_mover_boss_2:
        	
 	jr $31
 	
-#=============================================
-# - funcao para mover o boss enquanto personagem ta pulando
-mover_boss_personagem_pulando:
-	sw $31 0($29)
-       	addi $29 $29 -4
-	sw $8 0($29)
-       	addi $29 $29 -4
-       	sw $9 0($29)
-       	addi $29 $29 -4
-
-	addi $8 $0 18
-	div $4 $8
-	mfhi $9
-	beq $9 $0 mover_boss_1
-	addi $8 $0 9
-	beq $9 $8 mover_boss_2
-	addi $2 $4 1
-	
-	addi $29 $29 4                                                    
-       	lw $9 0($29)
-	addi $29 $29 4                                                    
-       	lw $8 0($29)
-       	addi $29 $29 4                                                    
-       	lw $31 0($29)
-	
-	jr $31
-	
 mover_boss_1_personagem_pulando:
 	jal desenhar_mapa_9
 	jal salvar_terceira_copia
@@ -36491,6 +36830,62 @@ continuacao_para_laco_cima_mover_boss_2_personagem_pulando:
        	
 	jr $31
 	
+#=============================================
+# - funcao para andar o bullet bill para a esquerda
+
+andar_bullet_bill_esquerda:
+	sw $31 0($29)
+       	addi $29 $29 -4
+	sw $8 0($29)
+       	addi $29 $29 -4
+       	sw $9 0($29)
+       	addi $29 $29 -4
+       	sw $10 0($29)
+       	addi $29 $29 -4
+       	sw $11 0($29)
+       	addi $29 $29 -4
+       	
+	addi $8 $4 -8
+	
+	add $4 $8 $0
+	jal desenhar_bullet_bill
+	
+	addi $8 $8 64
+	
+	li $9 16
+laco_andar_bullet_bill_esquerda_1:
+	li $11 2
+laco_andar_bullet_bill_esquerda_2:
+	
+	lw $10 131072($8)
+	sw $10 65536($8)
+	sw $10 0($8)
+	
+	addi $8 $8 4
+	addi $11 $11 -1
+	bnez $11 laco_andar_bullet_bill_esquerda_2
+	
+	addi $8 $8 -8
+	addi $8 $8 512
+	addi $9 $9 -1
+	bnez $9 laco_andar_bullet_bill_esquerda_1
+	
+	add $2 $4 $0
+	
+	jal timer_mob
+	
+	addi $29 $29 4                                                    
+       	lw $11 0($29)
+	addi $29 $29 4                                                    
+       	lw $10 0($29)
+	addi $29 $29 4                                                    
+       	lw $9 0($29)
+       	addi $29 $29 4                                                    
+       	lw $8 0($29)
+       	addi $29 $29 4                                                    
+       	lw $31 0($29)
+       	
+       	jr $31
        	
 #=============================================
 # funcao timer
